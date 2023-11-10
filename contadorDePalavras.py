fcbc874fef57aef0b032d35ff50dc1c617e0e5fe
Diego Ramos <93486526+Diego-7@users.noreply.github.com>
2023-11-10 10:34:32 -0300

    alteração no contador de palavras

--- contadorDePalavras.py
+++ contadorDePalavras.py
@@ -1,8 +1,6 @@
 texto = input("Insira o texto: ")
 
 def tratamento(texto):
-    texto = texto.replace(" ", ",").split(",")
-    return texto
-print
-print("Seu texto possui: " + str(len(tratamento(texto))) + " palavras")
+    return texto.split()
 
+print("Seu texto possui: " + str(len(tratamento(texto))) + " palavras")
