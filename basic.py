b0c87ec228fdd5c106e59e26ff6240dcd387e68e
Diego Ramos <93486526+Diego-7@users.noreply.github.com>
2023-11-12 10:09:02 -0300

    adição de algumas funções

--- basic.py
+++ basic.py
@@ -1,3 +1,5 @@
+from functools import reduce
+
 #variaveis
 num = 8
 name = "Diego"
@@ -5,3 +7,21 @@ lista = ["gol", "tiggo", "uno", "saveiro", "compass"]
 
 #funções
 
+#adiciona novo item no fim da lista
+def adicionar_item(lista):
+    novo = input("Insira o item: ")
+    lista.append(novo)
+adicionar_item(lista)    
+print(lista)
+
+#Soma todos os numeros da lista
+def add_num(a,b):
+    return a+b
+a = [1, 2, 3, 10]
+#print(reduce(add_num, a))
+#for i in range(5, 15, 3):
+ #   print(i) 
+
+#Retorna o indice e o item da lista
+#for indice, item in enumerate(lista):
+ #   print(f"índice {indice}: {item}")
--- pandas.py
+++ pandas.py
@@ -1,2 +1,9 @@
 import pandas as pd
+from datetime import date
+from datetime import datetime as dt
 
+#criando Series 
+
+pd.Series(data=5)
+
+lista_nomes = 'Diego Carol Ana Marcos Jaqueline'
